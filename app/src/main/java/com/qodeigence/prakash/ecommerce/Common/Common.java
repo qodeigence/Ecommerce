a5f1555ca54f674ec5e1b755d22b53ef67ec9b2e
prakashsurywanshi <prakashsurywanshi1@live.com>
2019-03-25 00:03:23 +0530

    second_time

---
+++ app/src/main/java/com/qodeigence/prakash/ecommerce/Common/Common.java
@@ -0,0 +1,7 @@
+package com.qodeigence.prakash.ecommerce.Common;
+
+import com.qodeigence.prakash.ecommerce.Model.User;
+
+public class Common {
+    public static User currentUser;
+}
---
+++ app/src/main/java/com/qodeigence/prakash/ecommerce/SignIn.java
@@ -0,0 +1,77 @@
+package com.qodeigence.prakash.ecommerce;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import com.qodeigence.prakash.ecommerce.Common.Common;
+import com.qodeigence.prakash.ecommerce.Model.User;
+
+public class SignIn extends AppCompatActivity {
+
+    EditText edtPhone,edtPassword;
+    Button btnSignIn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+
+        edtPassword = (EditText)findViewById(R.id.edtPassword);
+        edtPhone = (EditText)findViewById(R.id.edtPhone);
+        btnSignIn = (Button)findViewById(R.id.btnSignIn);
+
+        //Initialize Database
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference table_user = database.getReference("User");
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               final ProgressDialog mDialog = new ProgressDialog(SignIn.this);
+                mDialog.setMessage("Please Waiting...");
+                mDialog.show();
+
+                table_user.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                        if (dataSnapshot.child(edtPhone.getText().toString()).exists()) {
+                            // Get user information
+                            mDialog.dismiss();
+                            User user = dataSnapshot.child(edtPhone.getText().toString()).getValue(User.class);
+                            if (user.getPassword().equals(edtPassword.getText().toString())) {
+                                Intent homeIntent = new Intent(SignIn.this,Home.class);
+                                Common.currentUser = user;
+                                startActivity(homeIntent);
+                                finish();
+                            } else {
+                                Toast.makeText(SignIn.this, "Wrong Password !!!", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                        else{
+                            mDialog.dismiss();
+                            Toast.makeText(SignIn.this, "User not exists in database", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+            }
+        });
+    }
+}
