90d22f3288353500e6106782c46d3b4292ffcf10
prakashsurywanshi <prakashsurywanshi1@live.com>
2019-04-03 23:17:47 +0530

    Client_Done

--- app/src/main/java/com/qodeigence/prakash/ecommerce/FoodList.java
+++ app/src/main/java/com/qodeigence/prakash/ecommerce/FoodList.java
@@ -1,30 +1,30 @@
 package com.qodeigence.prakash.ecommerce;
 
 import android.content.Intent;
-//import android.support.annotation.NonNull;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-//import android.text.Editable;
-//import android.text.TextWatcher;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Toast;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
-//import com.google.firebase.database.DataSnapshot;
-//import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-//import com.google.firebase.database.ValueEventListener;
-//import com.mancj.materialsearchbar.MaterialSearchBar;
+import com.google.firebase.database.ValueEventListener;
+import com.mancj.materialsearchbar.MaterialSearchBar;
 import com.qodeigence.prakash.ecommerce.Interface.ItemClickListener;
 import com.qodeigence.prakash.ecommerce.Model.Food;
 import com.qodeigence.prakash.ecommerce.ViewHolder.FoodViewHolder;
 import com.squareup.picasso.Picasso;
 
-//import java.util.ArrayList;
-//import java.util.List;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FoodList extends AppCompatActivity {
 
@@ -37,14 +37,14 @@ public class FoodList extends AppCompatActivity {
 
     FirebaseRecyclerAdapter<Food,FoodViewHolder> adapter;
 
-/*
+
     //Search Functionality
 
     FirebaseRecyclerAdapter<Food,FoodViewHolder> SearchAdapter;
     List<String> suggestList = new ArrayList<>();
     MaterialSearchBar materialSearchBar;
 
-*/
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -66,7 +66,7 @@ public class FoodList extends AppCompatActivity {
         {
             loadListFood(categoryId);
         }
-/*
+
         //Search
 
         materialSearchBar = (MaterialSearchBar)findViewById(R.id.searchBar);
@@ -175,7 +175,7 @@ public class FoodList extends AppCompatActivity {
                     }
                 });
 
-*/
+
     }
 
     private void loadListFood(String categoryId) {
